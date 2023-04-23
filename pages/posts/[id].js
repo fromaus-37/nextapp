7c1e6b99b6b23bbbcd36ababaef4a56caafd7708
fromaus-37 <naveed_ausaf@hotmail.com>
2023-04-23 18:52:14 +0100

    done

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import utilStyles from "../../styles/utils.module.css";
+
+import Layout from "../../components/layout";
+import Head from "next/head";
+
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}></Date>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const result = {
+    paths: getAllPostIds(),
+    fallback: false,
+  };
+  return result;
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
